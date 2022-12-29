b4ad744b57533f75dc26b06a9e5c65d9a1b2b95b
WillOrtiz7 <willortiz407@gmail.com>
2022-12-28 19:48:07 -0500

    AQ-261: Add UserContext to track the info of the logged in user, create custom button component to make buttons modular

---
+++ src/components/CustomButton.js
@@ -0,0 +1,25 @@
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import { Theme } from "../constants/Theme";
+
+export default function CustomButton(props) {
+  return (
+    <TouchableOpacity onPress={props.action} style={styles.button}>
+      <Text style={styles.buttonText}>{props.label}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    ...Theme.button,
+  },
+  buttonText: {
+    fontSize: Theme.text.fontSizeButton,
+    color: Theme.text.color,
+  },
+});
---
+++ src/components/HomeScreen.js
@@ -0,0 +1,48 @@
+import { useContext, useState, useEffect } from "react";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import { UserContext } from "../constants/UserContext";
+import CustomButton from "./CustomButton";
+import { Auth } from "aws-amplify";
+
+export default function HomeScreen({ navigation }) {
+  const { userInfo, setUserInfo } = useContext(UserContext);
+  const [loading, setLoading] = useState(true);
+
+  // Reload the component when the user info is fetched
+  useEffect(() => {
+    console.log("User info updated");
+    setLoading(false);
+  }, [userInfo]);
+
+  // Clear the user info and sign out 
+  function signOut() {
+    setUserInfo({});
+    Auth.signOut();
+  }
+
+  return (
+    <View style={styles.container}>
+      {loading ? (
+        <ActivityIndicator size="large" color="#000000"></ActivityIndicator>
+      ) : (
+        <Text style={styles.text}>
+          Welcome to the home screen!{" "}
+          {JSON.stringify(userInfo?.attributes?.email)}
+        </Text>
+      )}
+      <CustomButton label="Sign Out" action={signOut} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
