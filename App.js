be0aa7c278ab534bbd913461c57775aa5fd6e457
WillOrtiz7 <willortiz407@gmail.com>
2023-01-10 10:39:05 -0500

    AQ-261: Update the method of getting the current logged in user

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { useState, useEffect, useMemo } from "react";
 import { StyleSheet } from "react-native";
-import { Amplify, Auth } from "aws-amplify";
+import { Amplify } from "aws-amplify";
 import config from "./src/authentication/aws-exports";
 import { withAuthenticator } from "aws-amplify-react-native";
+import { Authenticator } from "@aws-amplify/ui-react-native";
 import { signUpConfig } from "./src/authentication/SignUpConfig";
 import { CustomAuthTheme } from "./src/constants/CustomAuthTheme";
-import { UserContext } from "./src/constants/UserContext";
 import { NavigationContainer } from "@react-navigation/native";
 import BottomTabNavigaton from "./src/components/BottomTabNavigaton";
 import { Theme } from "./src/constants/Theme";
@@ -14,35 +13,12 @@ import { Theme } from "./src/constants/Theme";
 Amplify.configure({ ...config, Analytics: { disabled: true } });
 
 function App() {
-  const [userInfo, setUserInfo] = useState(null);
-
-  // Update the data in our UserContext
-  const value = useMemo(
-    () => ({ userInfo, setUserInfo }),
-    [userInfo, setUserInfo]
-  );
-
-  // Get the logged in user info and store it
-  async function getCurrentUser() {
-    try {
-      const user = await Auth.currentAuthenticatedUser();
-      console.log("Successfully logged in as: ", user.attributes.email);
-      setUserInfo(user);
-    } catch (error) {
-      console.log("Error getting current user: ", error);
-    }
-  }
-  // Run the function to get the logged in user info upon opening the app
-  useEffect(() => {
-    getCurrentUser();
-  }, []);
-
   return (
-    <UserContext.Provider value={value}>
+    <Authenticator.Provider>
       <NavigationContainer>
         <BottomTabNavigaton />
       </NavigationContainer>
-    </UserContext.Provider>
+    </Authenticator.Provider>
   );
 }
 
--- src/components/main_screens/HomeScreen.js
+++ src/components/main_screens/HomeScreen.js
@@ -1,39 +1,28 @@
-import React, { useContext, useState, useEffect } from "react";
-import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
-import { UserContext } from "../../constants/UserContext";
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { useAuthenticator } from "@aws-amplify/ui-react-native";
 import CustomButton from "../CustomButton";
 import { Auth } from "aws-amplify";
 import { Theme } from "../../constants/Theme";
 
 export default function HomeScreen({ navigation }) {
-  const { userInfo, setUserInfo } = useContext(UserContext);
-  const [loading, setLoading] = useState(true);
+  // Get the current user, refresh app state only if the user changes
+  const { user } = useAuthenticator((context) => [context.user]);
 
-  // Reload the component when the user info is fetched
-  useEffect(() => {
-    setLoading(false);
-  }, [userInfo]);
-
-  // Clear the user info and sign out
   async function signOut() {
     try {
       await Auth.signOut();
     } catch (error) {
       console.log("error signing out: ", error);
     }
-    setUserInfo({});
   }
 
   return (
     <View style={styles.container}>
-      {loading ? (
-        <ActivityIndicator size="large" color="#000000"></ActivityIndicator>
-      ) : (
-        <Text style={styles.text}>
-          Welcome to the home screen!{" "}
-          {JSON.stringify(userInfo?.attributes?.email)}
-        </Text>
-      )}
+      <Text style={styles.text}>
+        Welcome to the home screen! {user?.attributes?.email}
+      </Text>
+
       <View style={{ position: "absolute", top: 60, right: 1 }}>
         <CustomButton label="Sign Out" action={signOut} />
       </View>
