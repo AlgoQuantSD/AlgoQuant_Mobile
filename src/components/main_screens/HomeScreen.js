c95c53587a4b8bfbfc318aab8b0a7580f8d09e60
WillOrtiz7 <willortiz407@gmail.com>
2023-02-09 16:54:33 -0500

    AQ-375: Add the your assets section of the home screen

--- src/components/main_screens/HomeScreen.js
+++ src/components/main_screens/HomeScreen.js
@@ -1,31 +1,95 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, ScrollView, StyleSheet } from "react-native";
 import { useAuthenticator } from "@aws-amplify/ui-react-native";
 import { THEME } from "../../constants/Theme";
+import { timeframeEnums } from "../../constants/graphEnums";
+import CustomGraph from "../reusable_components/CustomGraph";
+import GraphDetailsHeader from "../reusable_components/GraphDetailsHeader";
 
 export default function HomeScreen({ navigation }) {
   // Get the current user and only refresh the component if user is updated
   const { user } = useAuthenticator((context) => [context.user]);
 
+  // Filler data until we connect to the backend
+  const mockData1 = [
+    { x: 1, y: 2 },
+    { x: 2, y: 3 },
+    { x: 3, y: 5 },
+    { x: 4, y: 4 },
+    { x: 5, y: 7 },
+  ];
+  const mockData2 = [
+    { x: 2, y: 6 },
+    { x: 3, y: 2 },
+    { x: 4, y: 9 },
+    { x: 6, y: 2 },
+    { x: 8, y: 1 },
+  ];
+
+  const mockData3 = [
+    { x: 0, y: 6 },
+    { x: 5, y: 10 },
+    { x: 6, y: 7 },
+    { x: 7, y: 9 },
+    { x: 8, y: 12 },
+  ];
+
+  const mockData4 = [
+    { x: 0, y: 6 },
+    { x: 5, y: 1 },
+    { x: 6, y: 7 },
+    { x: 7, y: 4 },
+    { x: 8, y: 10 },
+  ];
+
+  const portfolioData = {
+    recentPrice: 152.01,
+    open: 150.64,
+    high: 153.19,
+    low: 150.64,
+    yearlyHigh: 176.15,
+    yearlyLow: 124.17,
+    priceDifferenceRaw: 1.47,
+    priceDifferencePercent: 0.8,
+  };
+
+  const [selectedTimeframe, setSelectedTimeframe] = useState(
+    timeframeEnums.DAY
+  );
+  const [graphData, setGraphData] = useState(mockData1);
+
+  // Update graphdata and change the selected timeframe
+  function handleTimeframeChange(timeframe) {
+    setSelectedTimeframe(timeframe);
+    switch (timeframe) {
+      case timeframeEnums.DAY:
+        setGraphData(mockData1);
+        break;
+      case timeframeEnums.FIVE:
+        setGraphData(mockData2);
+        break;
+      case timeframeEnums.MONTH:
+        setGraphData(mockData3);
+        break;
+      case timeframeEnums.YEAR:
+        setGraphData(mockData4);
+        break;
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        Welcome to the home screen! {user?.attributes?.email}
-      </Text>
-      <View style={styles.carouselContainer}>
-        <Text
-          style={styles.text}
-          onPress={() => navigation.navigate("InvestorScreen")}
-        >
-          InvestorScreen
-        </Text>
-        <Text
-          style={styles.text}
-          onPress={() => navigation.navigate("JobScreen")}
-        >
-          JobScreen
-        </Text>
-      </View>
+      <ScrollView>
+        <GraphDetailsHeader
+          graphTitle="Your Assets"
+          graphTrendData={portfolioData}
+          selectedTimeframe={selectedTimeframe}
+        />
+        <CustomGraph
+          graphData={graphData}
+          selectedTimeframe={selectedTimeframe}
+          handleTimeframeChange={handleTimeframeChange}
+        />
+      </ScrollView>
     </View>
   );
 }
@@ -33,21 +97,12 @@ export default function HomeScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start",
+    paddingTop: "10%",
     backgroundColor: THEME.colors.background,
   },
   text: {
     fontSize: THEME.text.fontSizeBody,
     color: THEME.text.color,
-  },
-  carouselContainer: {
-    height: "10%",
-    width: "75%",
-    display: "flex",
-    flexDirection: "row",
-    padding: "5%",
-    alignItems: "center",
-    justifyContent: "space-between",
   },
 });
