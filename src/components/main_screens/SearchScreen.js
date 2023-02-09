57d517c825cf75923cd1a617e2ac389ddf1ecdc4
Tim Yang <53491292+timanese@users.noreply.github.com>
2023-02-09 12:25:41 -0500

    AQ-280: SEARCH STOCK ENDPOINT IMPLEMENTED (#15)
    * endpoint implemented
    
    * check for empty query
    
    * landing screen for search
    
    * more process
    
    * removing prints
    
    * reusable search
    
    * pagename prop
    
    * search query moved to customsearch
    
    * comments'
    
    * comments 2
    
    ---------
    
    Co-authored-by: ryPattillo <ryan_pattillo@knights.ucf.edu>

--- src/components/main_screens/SearchScreen.js
+++ src/components/main_screens/SearchScreen.js
@@ -1,12 +1,57 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { THEME } from "../../constants/Theme";
 import CustomSearch from "../reusable_components/CustomSearch";
+import AlgoquantApiContext from "../../constants/ApiContext";
+import StockInfoScreen from "../nested_screens/search/StockInfoScreen";
 
 export default function SearchScreen({ navigation }) {
+  // State variable to hold the values of the latest query
+  const [searchResults, setSearchResults] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(false);
+  // State variables used to access algoquant SDK API and display/ keep state of user data from database
+  const algoquantApi = useContext(AlgoquantApiContext);
+  // Filter results whenever we change the query
+
+  // handle action when user selects a stock from the list of queries
+  function onSelectStock(item) {
+    console.log(item);
+    !searchResults.includes("Ticker not found")
+      ? navigation.navigate("StockInfoScreen", {
+          stockName: item,
+        })
+      : null;
+  }
+
+  // Api call to search-stock endpoint and responds with an array of tickers based on the query
+  function getSearchResults(searchQuery) {
+    if (algoquantApi.token && searchQuery !== "") {
+      setIsLoading(true);
+      algoquantApi
+        .searchStock(searchQuery)
+        .then((resp) => {
+          setSearchResults(resp.data["stock-tickers"]);
+          setIsLoading(false);
+          if (resp.data["stock-tickers"].length === 0) {
+            setSearchResults(["Ticker not found"]);
+          }
+        })
+        .catch((err) => {
+          // TODO: Need to implement better error handling
+          console.log(err);
+        });
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <CustomSearch navigation={navigation} searchType="standard" />
+      <CustomSearch
+        onSelectStock={onSelectStock}
+        isLoading={isLoading}
+        searchResults={searchResults}
+        getSearchResults={getSearchResults}
+      />
     </View>
   );
 }
