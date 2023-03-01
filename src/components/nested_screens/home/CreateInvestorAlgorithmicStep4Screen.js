d65298c50eedc013391a590ff5633f9e3f717b71
WillOrtiz7 <willortiz407@gmail.com>
2023-03-01 12:48:50 -0500

    AQ-387: Add investor configuration to the step 4 screen

--- src/components/nested_screens/home/CreateInvestorAlgorithmicStep4Screen.js
+++ src/components/nested_screens/home/CreateInvestorAlgorithmicStep4Screen.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import { ThemeProvider, useNavigation } from "@react-navigation/native";
 import { Button } from "react-native-paper";
+import { PERIOD_LIST } from "../../../constants/CreateInvestorConstants";
 import { THEME } from "../../../constants/Theme";
 
 export default function CreateInvestorAlgorithmicStep4Screen(props) {
@@ -9,8 +10,40 @@ export default function CreateInvestorAlgorithmicStep4Screen(props) {
   console.log("Step 4: ", investorObject);
   const navigation = useNavigation();
   return (
-    <View>
-      <Text>Create investor screen step 4</Text>
+    <View style={styles.container}>
+      {/* Header */}
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerText}>Confirm Investor Creation</Text>
+      </View>
+      <View style={styles.investorConfigurationContainer}>
+        <Text style={styles.sectionTitleText}>Investor Configuration</Text>
+        <View style={styles.investorConfigurationItem}>
+          <Text style={styles.text}>Investor name:</Text>
+          <Text style={styles.text}>{investorObject.investor_name}</Text>
+        </View>
+        <View style={styles.investorConfigurationItem}>
+          <Text style={styles.text}>Investor type:</Text>
+          <Text style={styles.text}>{investorObject.type}</Text>
+        </View>
+        <View style={styles.investorConfigurationItem}>
+          <Text style={styles.text}>Profit stop:</Text>
+          <Text style={styles.text}>{investorObject.profit_stop}%</Text>
+        </View>
+        <View style={styles.investorConfigurationItem}>
+          <Text style={styles.text}>Loss stop:</Text>
+          <Text style={styles.text}>{investorObject.loss_stop}%</Text>
+        </View>
+        <View style={styles.investorConfigurationItem}>
+          <Text style={styles.text}>Trade frequency:</Text>
+          <Text style={styles.text}>
+            {PERIOD_LIST.map((item) => {
+              if (item.value === investorObject.period) {
+                return item.name;
+              }
+            })}
+          </Text>
+        </View>
+      </View>
       <Button
         buttonColor={THEME.button.primaryColorBackground}
         textColor={THEME.text.secondaryColor}
@@ -22,4 +55,38 @@ export default function CreateInvestorAlgorithmicStep4Screen(props) {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: "5%",
+    marginLeft: "5%",
+    marginRight: "5%",
+  },
+  headerContainer: {
+    flex: 0.1,
+    justifyContent: "center",
+    backgroundColor: "blue",
+  },
+  text: { fontSize: THEME.text.fontSize.body, color: THEME.text.color.primary },
+  headerText: {
+    fontSize: THEME.text.fontSize.H3,
+    color: THEME.text.color.primary,
+  },
+  sectionTitleText: {
+    fontSize: THEME.text.fontSize.H4,
+    color: THEME.text.color.primary,
+    paddingTop: "2%",
+    paddingBottom: "2%",
+  },
+  investorConfigurationContainer: {
+    flex: 0.25,
+    justifyContent: "center",
+    backgroundColor: "red",
+  },
+  investorConfigurationItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingTop: "1%",
+    paddingBottom: "1%",
+  },
+});
