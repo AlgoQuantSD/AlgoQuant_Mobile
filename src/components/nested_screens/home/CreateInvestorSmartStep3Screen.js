c36c63404a28505afb721a0128497d105b332bcb
WillOrtiz7 <willortiz407@gmail.com>
2023-03-03 12:49:46 -0500

    AQ-387: Add create AI investor step 3 screen

--- src/components/nested_screens/home/CreateInvestorSmartStep3Screen.js
+++ src/components/nested_screens/home/CreateInvestorSmartStep3Screen.js
@@ -11,25 +11,91 @@ export default function CreateInvestorSmartStep3Screen(props) {
   console.log("AI investor: ", investorObject);
 
   return (
-    <View>
-      <Text>Create smart investor screen step 3</Text>
-      <Text>
-        Investor name: {investorObject.name} Investor type:{" "}
-        {investorObject.type}
-      </Text>
-      <Button
-        buttonColor={THEME.button.primaryColorBackground}
-        textColor={THEME.text.secondaryColor}
-        onPress={() =>
-          navigation.navigate("HomeScreen", {
-            investorObject: investorObject,
-          })
-        }
-      >
-        Next
-      </Button>
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerText}>Confirm Investor Creation</Text>
+      </View>
+      {/* Investor Configuration */}
+      <View style={styles.investorConfigurationContainer}>
+        <Text style={styles.sectionTitleText}>Investor Configuration</Text>
+        <View style={styles.investorConfigurationItem}>
+          <Text style={styles.text}>Investor name:</Text>
+          <Text style={styles.text}>{investorObject.investor_name}</Text>
+        </View>
+        <View style={styles.investorConfigurationItem}>
+          <Text style={styles.text}>Investor type:</Text>
+          <Text style={styles.text}>{investorObject.type}</Text>
+        </View>
+        <View style={styles.investorConfigurationItem}>
+          <Text style={styles.text}>Profit stop:</Text>
+          <Text style={styles.text}>{investorObject.profit_stop}%</Text>
+        </View>
+        <View style={styles.investorConfigurationItem}>
+          <Text style={styles.text}>Loss stop:</Text>
+          <Text style={styles.text}>{investorObject.loss_stop}%</Text>
+        </View>
+      </View>
+      {/* Create Investor Button */}
+      <View style={styles.nextButtonContainer}>
+        <Button
+          buttonColor={THEME.button.primaryColorBackground}
+          textColor={THEME.text.secondaryColor}
+          onPress={() =>
+            navigation.navigate("HomeScreen", {
+              investorObject: investorObject,
+            })
+          }
+        >
+          Create Investor
+        </Button>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingLeft: "5%",
+    paddingRight: "5%",
+    backgroundColor: THEME.colors.background,
+  },
+  text: {
+    fontSize: THEME.text.fontSize.body,
+    color: THEME.text.color.primary,
+  },
+  sectionTitleText: {
+    fontSize: THEME.text.fontSize.H4,
+    color: THEME.text.color.primary,
+    paddingTop: "2%",
+    paddingBottom: "2%",
+  },
+  headerContainer: {
+    flex: 0.1,
+    justifyContent: "center",
+  },
+  headerText: {
+    fontSize: THEME.text.fontSize.H3,
+    color: THEME.text.color.primary,
+  },
+  investorConfigurationContainer: {
+    flex: 0.25,
+    justifyContent: "center",
+  },
+  investorConfigurationItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingTop: "1%",
+    paddingBottom: "1%",
+  },
+  nextButtonContainer: {
+    flex: 0.6,
+    justifyContent: "flex-end",
+    paddingTop: "10%",
+    paddingBottom: "10%",
+    alignItems: "flex-end",
+  },
+  snackbarContainer: {
+    flex: 0.05,
+  },
+});
