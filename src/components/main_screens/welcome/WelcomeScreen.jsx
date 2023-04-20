2ed2f4f452d875a751cd2fa355db75c9e73ce3f8
willortiz7 <willortiz407@gmail.com>
2023-04-20 15:21:15 -0400

    Add skeleton for welcome screen slides

--- src/components/main_screens/welcome/WelcomeScreen.jsx
+++ src/components/main_screens/welcome/WelcomeScreen.jsx
@@ -1,13 +1,50 @@
-import { SafeAreaView, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { Button } from "react-native-paper";
+import Swiper from "react-native-swiper";
 import { THEME } from "../../../general_constants/theme/Theme";
 import { handleWelcomeScreenCompleted } from "./helpers/welcomScreenState";
 
 export default function WelcomeScreen(props) {
   const { setShowWelcomeScreen } = props;
   return (
-    <SafeAreaView>
-      <Text>Welcome Screen</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Welcome To AlgoQuant!</Text>
+      <Swiper
+        showsButtons={true}
+        autoplay={false}
+        loop={false}
+        activeDotColor={THEME.colors.primary}
+        buttonWrapperStyle={{ color: "red" }}
+        nextButton={
+          <Text>
+            <Ionicons
+              name="chevron-forward"
+              size={36}
+              color={THEME.colors.primary}
+            />
+          </Text>
+        }
+        prevButton={
+          <Text>
+            <Ionicons
+              name="chevron-back"
+              size={36}
+              color={THEME.colors.primary}
+            />
+          </Text>
+        }
+      >
+        <View>
+          <Text>Slide 1</Text>
+        </View>
+        <View>
+          <Text>Slide 2</Text>
+        </View>
+        <View>
+          <Text>Slide 3</Text>
+        </View>
+      </Swiper>
       <Button
         buttonColor={THEME.button.primaryColorBackground}
         textColor={THEME.text.color.secondary}
@@ -19,3 +56,7 @@ export default function WelcomeScreen(props) {
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, paddingLeft: "5%", paddingRight: "5%" },
+});
